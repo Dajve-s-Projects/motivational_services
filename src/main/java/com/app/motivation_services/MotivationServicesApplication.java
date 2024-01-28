f01a1d8e434f4f34882539f49ef1d78c7f742394
Dajve Echols <dajvelechols@gmail.com>
2024-01-28 12:02:47 -0600

    adding spring boilerplate

---
+++ src/main/java/com/app/motivation_services/MotivationServicesApplication.java
@@ -0,0 +1,13 @@
+package com.app.motivation_services;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MotivationServicesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MotivationServicesApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/app/motivation_services/MotivationServicesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.app.motivation_services;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MotivationServicesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
